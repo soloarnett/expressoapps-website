fc6670a86788cd5e53c6381eebf107af5e8e6eee
solo <solomonarnett@gmail.com>
2016-11-22 14:54:15 -0500

    beta url added

---
+++ public/assets/js/main.js
@@ -0,0 +1,87 @@
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+var bodyPos = $('body').scrollTop();
+var image1Pos = $('#image1-container').offset().top;
+var image2Pos = $('#image2-container').offset().top;
+var image3Pos = $('#image3-container').offset().top;
+var image4Pos = $('#image4-container').offset().top;
+var logoWidth = $('.main-logo').width();
+
+function resizeFunction(){
+	windowHeight = window.innerHeight;
+	windowWidth = window.innerWidth;
+	logoWidth = $('.main-logo').width();
+	// image1Pos = $('#image1-container').offset().top;
+	// image2Pos = $('#image2-container').offset().top;
+	// image3Pos = $('#image3-container').offset().top;
+	// image4Pos = $('#image4-container').offset().top;
+	logoSize();
+	// console.log("hello");
+}
+
+function logoSize(){
+	// console.log(logoWidth);
+	if (logoWidth > 500) {
+		$('.main-logo').attr('src', 'assets/img/logo-800.png');
+	}else if(logoWidth > 400){
+		$('.main-logo').attr('src', 'assets/img/logo-500.png');
+	}else if(logoWidth > 250){
+		$('.main-logo').attr('src', 'assets/img/logo-350.png');
+	}else{
+		$('.main-logo').attr('src', 'assets/img/logo-250.png');
+	}
+}
+
+function scrollFunction(){
+	bodyPos = $('body').scrollTop();
+	image1Pos = $('#image1-container').offset().top;
+	image2Pos = $('#image2-container').offset().top;
+	image3Pos = $('#image3-container').offset().top;
+	image4Pos = $('#image4-container').offset().top;
+	// $('body').css('background-color', '#000000');
+	// console.log("works");
+	if (image1Pos <= 0) {
+		$('#image1-content-container:not(.container-fixed)').addClass('container-fixed');
+		$('.main-logo-content').css('opacity', '0');
+		// console.log("works");
+	}else{
+		$('#image1-content-container').removeClass('container-fixed');
+		$('.main-logo-content').css('opacity', '1');
+	}
+	if (image2Pos <= 0) {
+		$('#image2-content-container:not(.container-fixed)').addClass('container-fixed');
+		// console.log("works");
+	}else{
+		$('#image2-content-container').removeClass('container-fixed');
+	}
+	if (image3Pos <= 0) {
+		$('#image3-content-container:not(.container-fixed)').addClass('container-fixed');
+		// console.log("works");
+	}else{
+		$('#image3-content-container').removeClass('container-fixed');
+	}
+	if (image4Pos <= 0) {
+		$('#image4-content-container:not(.container-fixed)').addClass('container-fixed');
+		// console.log("works");
+	}else{
+		$('#image4-content-container').removeClass('container-fixed');
+	}
+
+
+}
+
+
+// -----------------------------------------------------	DOCUMENT READY		-----------------------------------------------------
+
+$(document).ready(function(){
+	logoSize();
+
+	$('.spacer-30').css('opacity', '1');
+
+window.onresize = resizeFunction;
+	// $('body').on('resize', resizeFunction);
+	$('body').on('scroll', scrollFunction);
+	// window.onscroll = scrollFunction;
+// console.log("works");
+
+});
